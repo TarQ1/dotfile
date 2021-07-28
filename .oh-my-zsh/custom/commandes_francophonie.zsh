0872768c3b6edea8d45b6c1e6e9c5f93763bac1c
Sylvain Ecuer <sylvain.ecuer@epita.fr>
2021-07-29 00:22:09 +0200

    oh-my-zsh

---
+++ .oh-my-zsh/custom/commandes_francophonie.zsh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+##### Commandes pour coquillage appropriées pour la semaine de la francophonie.
+##### Version 1.0
+
+### Commandes usuelles
+alias dpl="mv" # Déplace
+alias supr="rm" # Supprime
+alias sortie="exit"
+alias ddt="pwd" # Dossier De Travail
+alias faisdos="mkdir"
+alias suprdos="rmdir"
+alias touche="touch"
+alias 'pousse-en-suivant-les-étiquettes'='git push --follow-tags'
+### Structures de contrôle
+alias si="if"
+alias is="fi"
+alias alors="then"
+alias sinon="else"
+
+alias pour="for"
+alias tantque="while"
+alias faire="do"
+alias fait="done"
+
+alias cas="case"
+alias sac="esac"
+
+alias vrai="true"
+alias faux="false"
+
+### Moteur de production
+alias fais="make"
+
+### Git
+git config --global alias.ajoute add
+git config --global alias.engager commit
+git config --global alias.branche branch
+git config --global alias.cachette stash
+git config --global alias.change switch
+git config --global alias.controle checkout
+git config --global alias.cueille-cerise cherry-pick
+git config --global alias.dichotomie bisect
+git config --global alias.distant remote
+git config --global alias.dpl mv
+git config --global alias.etiquette tag
+git config --global alias.fusionne merge
+git config --global alias.hisse pull
+git config --global alias.inverse revert
+git config --global alias.journal log
+git config --global alias.journalet shortlog
+git config --global alias.montre show
+git config --global alias.picore cherry-pick
+git config --global alias.pousse push
+git config --global alias.rapporte fetch
+git config --global alias.reinit reset
+git config --global alias.statut status
+git config --global alias.supr rm
+git config --global alias.tire pull
